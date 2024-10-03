c0b292a71f80422d751bbce88e1266a16e48f460
daniel zerihon <daniel92023@gmail.com>
2024-10-03 15:12:56 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerID=`docker ps | awk -F " " '{print $1}'`
+containerID=`sudo docker ps | awk -F " " '{print $1}' | tail -1`
 docker rm -f $containerID
